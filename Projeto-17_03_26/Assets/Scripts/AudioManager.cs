c58dc5fa3a63d939e0b750d8666b8a3bf7e8965a
MariaJulia1207 <maria.palhares@escolar.ifrn.edu.br>
2026-03-30 08:30:02 -0300

    30/03/2026

---
+++ Projeto-17_03_26/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Singelton
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+}
