8ee4a5e34cb41ea37876de57c39696673b687e5e
MariaJulia1207 <maria.palhares@escolar.ifrn.edu.br>
2026-03-31 10:24:09 -0300

    31/03/2026

--- Projeto-17_03_26/Assets/Scripts/AudioManager.cs
+++ Projeto-17_03_26/Assets/Scripts/AudioManager.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
+    private AudioSource systemSource;
+    private List<AudioSource> activeSources;
+
     #region Singelton
     public static AudioManager Instance;
 
@@ -12,11 +18,46 @@ public class AudioManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            systemSource = GetComponent<AudioSource>();
+            activeSources = new List<AudioSource>();
         }
         else
         {
             Destroy(gameObject);
         }
     }
+    #endregion
+    #region AudioControls
+
+    public void Play(AudioClip clip, AudioSource source)
+    {
+        if (!activeSources.Contains(source))
+            activeSources.Add(source);
+        systemSource.Stop();
+        systemSource.clip = clip;
+        systemSource.Play();
+    }
+
+    public void PlayOneShot(AudioClip clip,  AudioSource source)
+    {
+        source.PlayOneShot(clip);
+    }
+    public void Stop(AudioSource source)
+    {
+        if(activeSources.Contains(source))
+            source.Stop();
+        activeSources.Remove(source);
+    }
+    public void Pause(AudioSource source)
+    {
+        if (activeSources.Contains(source))
+             source.Pause();
+    }
+    public void Resume(AudioSource source)
+    {
+        if (activeSources.Contains(source)) 
+            source.UnPause();
+    }
+
     #endregion
 }
